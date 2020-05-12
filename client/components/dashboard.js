55b428d307c9091fd4a01cdd2f385c601259d997
Kim <mavrovki@gmail.com>
2020-05-12 20:42:10 +0300

    update

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Dashboard = () => {
   return (
     <div>
-      <div id="title"> Dashboard </div>
+      <div id="title">Dashboard</div>
       <Link to="/dashboard/profile/b7df2f70-6d20-4bd8-9eb7-521664ed95f3"> Go To Profile </Link>
       <Link to="/dashboard/main"> Go To Main </Link>
     </div>
